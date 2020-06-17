b74346b69c886061d1f8c62578314a5431e97f0e
raymawo <wout.raymaekers@faros.be>
2020-06-17 16:35:03 +0200

    Fixed the test. Making the build fail.

--- src/test/java/be/faros/samplecicdwithjenkins/controller/HomeControllerTest.java
+++ src/test/java/be/faros/samplecicdwithjenkins/controller/HomeControllerTest.java
@@ -1,12 +1,11 @@
 package be.faros.samplecicdwithjenkins.controller;
 
 import org.junit.jupiter.api.Test;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(HomeController.class)
@@ -16,7 +15,7 @@ class HomeControllerTest {
     MockMvc mockMvc;
 
     @Test
-    void home() throws Exception {
-        this.mockMvc.perform(get("/home")).andExpect(status().isIAmATeapot());
+    void home() throws Excption {
+        this.mockMvc.perform(get("/home")).andExpect(status().isOk());
     }
 }
